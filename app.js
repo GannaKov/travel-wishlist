50b0325418612e2bcb162f9db7a47d7b94ccc4ff
GannaKov <104867435+GannaKov@users.noreply.github.com>
2024-04-01 21:45:27 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const exampleRouter = require("./routes/example");
+//require('dotenv').config();
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+//app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//const path = require('path');
+
+app.use("/", exampleRouter);
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send(err.message);
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error " } = err;
+
+  res.status(status).json({ message });
+});
+
+module.exports = app;
